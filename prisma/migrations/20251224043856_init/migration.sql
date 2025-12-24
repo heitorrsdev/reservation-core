b837383613e6fcae8fc4cb3ac703565c20e85013
Heitor Rosa <heitorrs.dev@gmail.com>
2025-12-24 02:12:54 -0300

    chore: reset database schema and recreate initial migration

--- prisma/migrations/20251224043856_init/migration.sql
+++ prisma/migrations/20251224043856_init/migration.sql
@@ -1,21 +1,26 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('CLIENT', 'BARBER', 'ADMIN');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "passwordHash" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Resource" (
+CREATE TABLE "Barber" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "description" TEXT,
+    "bio" TEXT,
+    "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "Resource_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Barber_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -23,9 +28,10 @@ CREATE TABLE "Reservation" (
     "id" TEXT NOT NULL,
     "startTime" TIMESTAMP(3) NOT NULL,
     "endTime" TIMESTAMP(3) NOT NULL,
+    "period" tstzrange NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
-    "resourceId" TEXT NOT NULL,
+    "barberId" TEXT NOT NULL,
 
     CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
 );
@@ -34,10 +40,16 @@ CREATE TABLE "Reservation" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE INDEX "Reservation_resourceId_startTime_endTime_idx" ON "Reservation"("resourceId", "startTime", "endTime");
+CREATE INDEX "User_role_idx" ON "User"("role");
+
+-- CreateIndex
+CREATE INDEX "Barber_active_idx" ON "Barber"("active");
+
+-- CreateIndex
+CREATE INDEX "Reservation_barberId_idx" ON "Reservation"("barberId");
 
 -- AddForeignKey
 ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_resourceId_fkey" FOREIGN KEY ("resourceId") REFERENCES "Resource"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Reservation" ADD CONSTRAINT "Reservation_barberId_fkey" FOREIGN KEY ("barberId") REFERENCES "Barber"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
