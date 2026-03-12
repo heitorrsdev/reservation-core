634874315b9dde8899d604546592922e94a65f6e
Heitor Rosa <heitorrs.dev@gmail.com>
2026-03-12 16:07:33 -0300

    fix: update http path aliases and cleanup imports

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     '^@domain/(.*)$': '<rootDir>/src/domain/$1',
     '^@application/(.*)$': '<rootDir>/src/application/$1',
     '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
-    '^@http/(.*)$': '<rootDir>/src/http/$1',
+    '^@http/(.*)$': '<rootDir>/src/interfaces/http/$1',
     '^@test/(.*)$': '<rootDir>/test/$1',
   },
   preset: 'ts-jest',
