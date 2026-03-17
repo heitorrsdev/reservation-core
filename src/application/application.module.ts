d67426184846b05d5f1fe30f4d5ec33c543b2276
Heitor Rosa <heitorrs.dev@gmail.com>
2026-03-17 16:53:51 -0300

    feat: add CreateReservationUseCase to ApplicationModule

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -2,11 +2,12 @@ import { InfrastructureModule } from '@infrastructure/infrastructure.module';
 import { Module } from '@nestjs/common';
 
 import { CreateBarberUseCase } from './barber/create-barber.usecase';
+import { CreateReservationUseCase } from './reservation/create-reservation.usecase';
 import { CreateUserUseCase } from './user/create-user.usecase';
 
 @Module({
   imports: [InfrastructureModule],
-  providers: [CreateUserUseCase, CreateBarberUseCase],
-  exports: [CreateUserUseCase, CreateBarberUseCase],
+  providers: [CreateUserUseCase, CreateBarberUseCase, CreateReservationUseCase],
+  exports: [CreateUserUseCase, CreateBarberUseCase, CreateReservationUseCase],
 })
 export class ApplicationModule {}
