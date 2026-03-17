f7efba3cb978c5657c63b933bafa6141d2c23af2
Heitor Rosa <heitorrs.dev@gmail.com>
2026-03-17 18:21:39 -0300

    feat: map new domain errors to http status codes

--- src/interfaces/http/errors/domain-error-http.mapper.ts
+++ src/interfaces/http/errors/domain-error-http.mapper.ts
@@ -1,4 +1,6 @@
+import { BarberNotFoundError } from '@domain/barber/errors/barber-not-found.error';
 import { UserAlreadyBarberError } from '@domain/barber/errors/user-already-barber.error';
+import { DependencyNotFoundError } from '@domain/errors/dependency-not-found.error';
 import type { DomainError } from '@domain/errors/domain.error';
 import { InvalidReservationTimeError } from '@domain/reservation/errors/invalid-reservation-time.error';
 import { ReservationConflictError } from '@domain/reservation/errors/reservation-conflict.error';
@@ -9,13 +11,15 @@ import { UserNotFoundError } from '@domain/user/errors/user-not-found.error';
 import { HttpStatus } from '@nestjs/common';
 
 export const errorStatusMap: Record<string, HttpStatus> = {
-  [UserAlreadyExistsError.name]: HttpStatus.CONFLICT,
-  [UserAlreadyBarberError.name]: HttpStatus.CONFLICT,
-  [UserNotFoundError.name]: HttpStatus.NOT_FOUND,
-  [ReservationConflictError.name]: HttpStatus.CONFLICT,
-  [InvalidReservationTimeError.name]: HttpStatus.BAD_REQUEST,
+  [BarberNotFoundError.name]: HttpStatus.NOT_FOUND,
+  [DependencyNotFoundError.name]: HttpStatus.NOT_FOUND,
+  [InvalidReservationTimeError.name]: HttpStatus.UNPROCESSABLE_ENTITY,
   [InvalidUserEmailFormatError.name]: HttpStatus.BAD_REQUEST,
   [InvalidUserPasswordHashError.name]: HttpStatus.BAD_REQUEST,
+  [ReservationConflictError.name]: HttpStatus.CONFLICT,
+  [UserAlreadyBarberError.name]: HttpStatus.CONFLICT,
+  [UserAlreadyExistsError.name]: HttpStatus.CONFLICT,
+  [UserNotFoundError.name]: HttpStatus.NOT_FOUND,
 };
 
 export class DomainErrorHttpMapper {
