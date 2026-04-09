9c8532ada15a7ab2044871974af9c6d1db182c8d
Heitor Rosa <heitorrs.dev@gmail.com>
2026-04-09 15:23:29 -0300

    chore: add rate limiting with Throttler

--- src/interfaces/http/http.module.ts
+++ src/interfaces/http/http.module.ts
@@ -2,6 +2,7 @@ import { ApplicationModule } from '@application/application.module';
 import { JwtAuthGuard } from '@infrastructure/auth/jwt-auth.guard';
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 import { AuthController } from './auth/auth.controller';
 import { BarberController } from './barber/barber.controller';
@@ -11,7 +12,22 @@ import { ReservationController } from './reservation/reservation.controller';
 import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [ApplicationModule, HealthModule],
+  imports: [
+    ApplicationModule,
+    HealthModule,
+    ThrottlerModule.forRootAsync({
+      useFactory: () => [
+        {
+          ttl: 60000,
+          limit: process.env.THROTTLE_LIMIT
+            ? parseInt(process.env.THROTTLE_LIMIT, 10)
+            : process.env.NODE_ENV === 'test'
+              ? 10000
+              : 100,
+        },
+      ],
+    }),
+  ],
   controllers: [
     AuthController,
     UserController,
@@ -26,6 +42,10 @@ import { UserController } from './user/user.controller';
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
