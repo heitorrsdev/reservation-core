34226f48196d98a3cb0af22fe8d95443558d9cfd
Heitor Rosa <heitorrs.dev@gmail.com>
2026-04-17 21:52:28 -0300

    refactor: use port variable for app.listen

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port);
 }
 
 bootstrap().catch((error: unknown) => {
